ebc23074cb238494a73377165b593aaaa05509ce
rkhar <rassul.khar@gmail.com>
2021-09-27 15:10:48 +0600

    make caching optional

--- src/main/scala/scalac/http/GithubRoutes.scala
+++ src/main/scala/scalac/http/GithubRoutes.scala
@@ -17,7 +17,7 @@ import akka.http.scaladsl.server.RequestContext
 import akka.http.scaladsl.server.RouteResult
 import akka.http.scaladsl.model.Uri
 import akka.http.scaladsl.server.directives.CachingDirectives._
-import scala.concurrent.duration._
+
 import scala.concurrent.duration._
 
 trait GithubRoutes extends GithubHttpExceptionHandler {
@@ -69,42 +69,42 @@ trait GithubRoutes extends GithubHttpExceptionHandler {
 
   def repos(): Route = path("orgs" / Segment / "repos") { orgName =>
     get {
-      alwaysCache(lfuCache, simpleKeyer)(
+      cache(lfuCache, simpleKeyer)(
         complete(githubClient.getRepos(orgName)))
     }
   }
 
   def reposV2(): Route = path("orgs" / Segment / "repos") { orgName =>
     get {
-      alwaysCache(lfuCache, simpleKeyer)(
+      cache(lfuCache, simpleKeyer)(
         complete(githubClient.getReposV2(orgName)))
     }
   }
 
   def reposV3(): Route = path("orgs" / Segment / "repos") { orgName =>
     get {
-      alwaysCache(lfuCache, simpleKeyer)(
+      cache(lfuCache, simpleKeyer)(
         complete(githubClient.getReposV3(orgName)))
     }
   }
 
   def repoContributors(): Route = path("repos" / Segment / Segment / "contributors") { (owner, repo) =>
     get {
-      alwaysCache(lfuCache, simpleKeyer)(
+      cache(lfuCache, simpleKeyer)(
         complete(githubClient.getRepoContributors(owner, repo)))
     }
   }
 
   def repoContributorsV2(): Route = path("repos" / Segment / Segment / "contributors") { (owner, repo) =>
     get {
-      alwaysCache(lfuCache, simpleKeyer)(
+      cache(lfuCache, simpleKeyer)(
         complete(githubClient.getRepoContributorsV2(owner, repo)))
     }
   }
 
   def repoContributorsV3(): Route = path("repos" / Segment / Segment / "contributors") { (owner, repo) =>
     get {
-      alwaysCache(lfuCache, simpleKeyer)(
+      cache(lfuCache, simpleKeyer)(
         complete(githubClient.getRepoContributorsV3(owner, repo)))
     }
   }
@@ -112,21 +112,21 @@ trait GithubRoutes extends GithubHttpExceptionHandler {
 
   def orgContributors(): Route = path("orgs" / Segment / "contributors") { org =>
     get {
-      alwaysCache(lfuCache, simpleKeyer)(
+      cache(lfuCache, simpleKeyer)(
         complete(githubClient.getOrganizationContributors(org)))
     }
   }
 
   def orgContributorsV2(): Route = path("orgs" / Segment / "contributors") { org =>
     get {
-      alwaysCache(lfuCache, simpleKeyer)(
+      cache(lfuCache, simpleKeyer)(
         complete(githubClient.getOrganizationContributorsV2(org)))
     }
   }
 
   def orgContributorsV3(): Route = path("orgs" / Segment / "contributors") { org =>
     get {
-      alwaysCache(lfuCache, simpleKeyer)(
+      cache(lfuCache, simpleKeyer)(
         complete(githubClient.getOrganizationContributorsV3(org)))
     }
   }
